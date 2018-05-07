18fdc4efff4a55327c9b8e44333eb943d256afd8
Matti Pohjalainen <matti.pohjalainen@elisanet.fi>
2018-05-07 08:24:02 +0300

    fix to anecdote exercise

--- osa1/anecdotes/src/MostVotes.js
+++ osa1/anecdotes/src/MostVotes.js
@@ -1,41 +1,41 @@
 import React from "react";
 
-const MostVotes = (props) => {
+const MostVotes = props => {
+  const anecdotes = props.anecdotes;
+  const pinnat = props.pisteet;
 
-    const anecdotes = props.anecdotes;
-    const pinnat = props.pisteet;
+  const findIndexOfMostVoted = () => {
+    return pinnat.indexOf(Math.max(...pinnat));
+  };
 
-    const findIndexOfMostVoted = () => {
-        return pinnat.indexOf(Math.max(...pinnat));
-      }
-  
-      const findMostVoted = () => {
-        const indexOfVoted = findIndexOfMostVoted()
-        console.log("suosituin " + indexOfVoted)
-  
-        if (0 === pinnat.indexOf(indexOfVoted)) {
-          console.log("mitään ei vielä äänestetty...")
-          return '';
-        }
-  
-        return anecdotes[indexOfVoted]
-      }
-  
+  const findMostVoted = () => {
+    const indexOfVoted = findIndexOfMostVoted();
+    console.log("suosituin " + indexOfVoted);
 
-    if (pinnat[findIndexOfMostVoted()] === 0) {
-        return (
-            ''
-        );
-    } else {
-        return (
-            <div>
-            <h2>anecdote with most votes</h2>
-            <p>{findMostVoted()}</p>
-            <p>has {pinnat[findIndexOfMostVoted()]} votes</p>
-            </div>
-        );
+    if (0 === pinnat.indexOf(indexOfVoted)) {
+      console.log("mitään ei vielä äänestetty...");
+      return "";
     }
 
+    return anecdotes[indexOfVoted];
   };
-  
-  export default MostVotes;
+
+  if (pinnat[findIndexOfMostVoted()] === 0) {
+    return (
+      <div>
+        <h2>anecdote with most votes</h2>
+        <p>No notes so far</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>anecdote with most votes</h2>
+        <p>{findMostVoted()}</p>
+        <p>has {pinnat[findIndexOfMostVoted()]} votes</p>
+      </div>
+    );
+  }
+};
+
+export default MostVotes;
